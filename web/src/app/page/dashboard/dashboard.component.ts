cbc696fd5b797d5fc64b2e6b9a4e18d7aeceb9da
PomeloMan <fengchao.z@outlook.com>
2019-03-06 10:55:12 +0800

    Commit

--- web/src/app/page/dashboard/dashboard.component.ts
+++ web/src/app/page/dashboard/dashboard.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { FLY_IN_OUT, toggle } from '../../common/animations';
+import { CalendarView, CalendarEvent, CalendarEventTimesChangedEvent, CalendarEventAction } from 'angular-calendar';
+import { Subject } from 'rxjs';
+import {
+	startOfDay,
+	subDays,
+	addDays,
+	endOfMonth,
+	addHours,
+	isSameMonth,
+	isSameDay
+} from 'date-fns';
 
 @Component({
 	selector: 'app-dashboard',
@@ -31,4 +42,115 @@ export class DashboardComponent {
 		this.active = !this.active;
 		this.state = this.active ? 'active' : 'inactive';
 	}
+
+	// Calendar
+	actions: CalendarEventAction[] = [
+		{
+			label: '<i class="fa fa-fw fa-pencil"></i>',
+			onClick: ({ event }: { event: CalendarEvent }): void => {
+				this.handleEvent('Edited', event);
+			}
+		},
+		{
+			label: '<i class="fa fa-fw fa-times"></i>',
+			onClick: ({ event }: { event: CalendarEvent }): void => {
+				this.events = this.events.filter(iEvent => iEvent !== event);
+				this.handleEvent('Deleted', event);
+			}
+		}
+	];
+	CalendarView = CalendarView;
+	view: CalendarView = CalendarView.Month;
+	viewDate: Date = new Date();
+	events: CalendarEvent[] = [
+		{
+			start: subDays(startOfDay(new Date()), 1),
+			end: addDays(new Date(), 1),
+			title: 'A 3 day event',
+			color: colors.red,
+			actions: this.actions,
+			allDay: true,
+			resizable: {
+				beforeStart: true,
+				afterEnd: true
+			},
+			draggable: true
+		},
+		{
+			start: startOfDay(new Date()),
+			title: 'An event with no end date',
+			color: colors.yellow,
+			actions: this.actions
+		},
+		{
+			start: subDays(endOfMonth(new Date()), 3),
+			end: addDays(endOfMonth(new Date()), 3),
+			title: 'A long event that spans 2 months',
+			color: colors.blue,
+			allDay: true
+		},
+		{
+			start: addHours(startOfDay(new Date()), 2),
+			end: new Date(),
+			title: 'A draggable and resizable event',
+			color: colors.yellow,
+			actions: this.actions,
+			resizable: {
+				beforeStart: true,
+				afterEnd: true
+			},
+			draggable: true
+		}
+	];
+	refresh: Subject<any> = new Subject();
+
+	modalData: {
+		action: string;
+		event: CalendarEvent;
+	};
+
+	handleEvent(action: string, event: CalendarEvent): void {
+		this.modalData = { event, action };
+	}
+
+	activeDayIsOpen: boolean = true;
+	dayClicked({ date, events }: { date: Date; events: CalendarEvent[]; }): void {
+		if (isSameMonth(date, this.viewDate)) {
+			if (
+				(isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+				events.length === 0
+			) {
+				this.activeDayIsOpen = false;
+			} else {
+				this.activeDayIsOpen = true;
+			}
+			this.viewDate = date;
+		}
+	}
+
+	eventTimesChanged({
+		event,
+		newStart,
+		newEnd
+	}: CalendarEventTimesChangedEvent): void {
+		event.start = newStart;
+		event.end = newEnd;
+		this.handleEvent('Dropped or resized', event);
+		this.refresh.next();
+	}
 }
+
+const colors: any = {
+	red: {
+		primary: '#ad2121',
+		secondary: '#FAE3E3'
+	},
+	blue: {
+		primary: '#1e90ff',
+		secondary: '#D1E8FF'
+	},
+	yellow: {
+		primary: '#e3bc08',
+		secondary: '#FDF1BA'
+	}
+};
--- web/src/app/page/dashboard/dashboard.module.ts
+++ web/src/app/page/dashboard/dashboard.module.ts
@@ -5,6 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FormsModule } from '@angular/forms';
+
 const routes: Routes = [
 	{ path: '', component: DashboardComponent }
 ];
@@ -12,7 +16,12 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		CommonModule,
+		FormsModule,
 		MaterialModule,
+		CalendarModule.forRoot({
+			provide: DateAdapter,
+			useFactory: adapterFactory
+		  }),
 		RouterModule.forChild(routes)
 	],
 	declarations: [DashboardComponent],

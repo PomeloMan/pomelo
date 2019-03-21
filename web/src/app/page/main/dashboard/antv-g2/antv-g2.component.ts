605c450ce31aa05ddda230c560585e4e1d3f3ddb
PomeloMan <fengchao.z@outlook.com>
2019-03-21 17:41:27 +0800

    Commit

--- web/src/app/page/main/dashboard/antv-g2/antv-g2.component.ts
+++ web/src/app/page/main/dashboard/antv-g2/antv-g2.component.ts
@@ -1,37 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as G2 from '@antv/g2';
+import { format, subMinutes } from 'date-fns';
 
 @Component({
   selector: 'app-antv-g2',
   templateUrl: './antv-g2.component.html',
   styleUrls: ['./antv-g2.component.scss']
 })
-export class AntvG2Component implements OnInit {
+export class AntvG2Component implements OnInit, OnDestroy {
 
-  data = [
-    { genre: 'Sports', sold: 275 },
-    { genre: 'Strategy', sold: 115 },
-    { genre: 'Action', sold: 120 },
-    { genre: 'Shooter', sold: 350 },
-    { genre: 'Other', sold: 150 }
-  ];
-  chart: G2.Chart;
+  now: Date = new Date();
+
+  private chart: G2.Chart;
+  private _lineInterval: any;
 
   constructor() { }
 
   ngOnInit() {
-    // Step 1: 创建 Chart 对象
-    this.chart = new G2.Chart({
-      container: 'c1', // 指定图表容器 ID
-      width: 600, // 指定图表宽度
-      height: 300 // 指定图表高度
-    });
-    // Step 2: 载入数据源
-    this.chart.source(this.data);
-    // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
-    this.chart.interval().position('genre*sold').color('genre')
-    // Step 4: 渲染图表
-    this.chart.render();
+    this.renderLine();
   }
 
+  ngOnDestroy(): void {
+    if (this._lineInterval) {
+      clearInterval(this._lineInterval);
+    }
+  }
+
+  renderLine() {
+    this.chart = new G2.Chart({
+      container: 'line',
+      forceFit: true,
+      height: window.innerHeight,
+      animate: false
+    });
+
+    let time = this.now.getTime();
+    let lastTime = subMinutes(time, 1).getTime();
+    let data = [];
+    while (time > lastTime) {
+      let second = format(lastTime, 'mm:ss');
+      data.push({
+        second: second,
+        value: Math.random()
+      })
+      lastTime += 1000;
+    }
+    this.chart.source(data);
+    this.chart.tooltip({
+      crosshairs: {
+        type: 'cross'
+      }
+    });
+    this.chart.scale('value', {
+      alias: '销售额(万)'
+    });
+    this.chart.line().position('second*value');
+    this.chart.point().position('second*value').size(4).shape('circle').style({
+      stroke: '#fff',
+      lineWidth: 1
+    });
+    this.chart.render();
+    this._lineInterval = setInterval(() => {
+      const _data = data;
+      let second = format(time, 'mm:ss');
+      _data.shift();
+      _data.push({
+        second: second,
+        value: Math.random()
+      })
+      this.chart.changeData(_data);
+      time += 1000;
+    }, 1000);
+  }
 }
--- web/src/app/page/main/dashboard/echarts/echarts.component.ts
+++ web/src/app/page/main/dashboard/echarts/echarts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as echarts from 'echarts';
 import 'echarts/theme/macarons.js';
+import { subMinutes, format } from 'date-fns';
 
 @Component({
   selector: 'app-echarts',
@@ -9,15 +10,17 @@ import 'echarts/theme/macarons.js';
 })
 export class EchartsComponent implements OnInit {
 
+  now: Date = new Date();
+
   chartOption: any;
-  chart: echarts.ECharts;
+
+  private lineChart: echarts.ECharts;
+  private _lineInterval: any;
 
   constructor() {
   }
 
   ngOnInit() {
-    var el: any = document.getElementById('echart');
-    var myChart = echarts.init(el, 'macarons');
     this.chartOption = {
       title: {
         text: 'ECharts 入门示例'
@@ -33,7 +36,68 @@ export class EchartsComponent implements OnInit {
         data: [5, 20, 36, 10, 10, 20]
       }]
     }
-    // 绘制图表
-    myChart.setOption(this.chartOption);
+    this.renderLine();
+  }
+
+  ngOnDestroy(): void {
+    if (this._lineInterval) {
+      clearInterval(this._lineInterval);
+    }
+  }
+
+  renderLine() {
+    let el: any = document.getElementById('echart');
+
+    let time = this.now.getTime();
+    let lastTime = subMinutes(time, 1).getTime();
+    let xdata = [];
+    let sdata = [];
+    while (time > lastTime) {
+      let second = format(lastTime, 'mm:ss');
+      xdata.push(second);
+      sdata.push(Math.random());
+      lastTime += 1000;
+    }
+
+    let option: echarts.EChartOption = {
+      animation: false,
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        }
+      },
+      xAxis: {
+        name: '时间（分：秒）',
+        type: 'category',
+        data: xdata
+      },
+      yAxis: {
+        name: '销售额（万）',
+        type: 'value'
+      },
+      series: [{
+        data: sdata,
+        type: 'line'
+      }]
+    };
+    this.lineChart = echarts.init(el, 'macarons', { width: window.innerWidth, height: window.innerHeight });
+    this.lineChart.setOption(option);
+
+    this._lineInterval = setInterval(() => {
+      let _xdata = xdata;
+      let _sdata = sdata;
+      let second = format(time, 'mm:ss');
+      _xdata.shift();
+      _xdata.push(second);
+      _sdata.shift();
+      _sdata.push(Math.random());
+
+      let _option: any = this.lineChart.getOption();
+      _option.xAxis[0].data = _xdata;
+      _option.series[0].data = _sdata;
+      this.lineChart.setOption(_option);
+      time += 1000;
+    }, 1000);
   }
 }
